26d4cd14e52492caa9f3b8cec1330c7156c5ef68
Matheus Rodrigues <matheus.moreira.rodrigues01@gmail.com>
2018-06-25 23:07:38 -0300

    Refactoring

--- SimpleToDo.Web.IntegrationTest/WebFixture.cs
+++ SimpleToDo.Web.IntegrationTest/WebFixture.cs
@@ -1,43 +1,39 @@
 using System;
 using System.Net.Http;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.AspNetCore.TestHost;
 using Microsoft.EntityFrameworkCore.Storage;
 using SimpleToDo.Model.Entities;
-using SimpleToDo.Web;
 
-public class WebFixture<TStartup> : IDisposable where TStartup : class
+namespace SimpleToDo.Web.IntegrationTest
 {
-    private readonly WebApplicationFactory<TStartup> _factory;
-    private readonly IServiceProvider _services;
-    private readonly IDbContextTransaction _transaction;
-
-    protected HttpClient Client;
-    protected ToDoDbContext DbContext { get; }
-
-    public WebFixture()
-    {        
-        _factory = new WebApplicationFactory<TStartup>();
-        Client = _factory.CreateClient(new WebApplicationFactoryClientOptions
-                {
-                    AllowAutoRedirect = false
-                });
-
-        _services = _factory.Server.Host.Services;
-
-        DbContext = GetService<ToDoDbContext>();
-        _transaction = DbContext.Database.BeginTransaction();
-    }
-
-    protected T GetService<T>() => (T)_services.GetService(typeof(T));
-
-    public void Dispose()
+    public class WebFixture<TStartup> : IDisposable where TStartup : class
     {
-        if (_transaction != null)
+        private readonly IServiceProvider _services;
+        private readonly IDbContextTransaction _transaction;
+
+        protected HttpClient Client;
+        protected ToDoDbContext DbContext { get; }
+
+        public WebFixture()
         {
+            var factory = new WebApplicationFactory<TStartup>();
+            Client = factory.CreateClient(new WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = false
+            });
+
+            _services = factory.Server.Host.Services;
+
+            DbContext = GetService<ToDoDbContext>();
+            _transaction = DbContext.Database.BeginTransaction();
+        }
+
+        protected T GetService<T>() => (T)_services.GetService(typeof(T));
+
+        public void Dispose()
+        {
+            if (_transaction == null) return;
+
             _transaction.Rollback();
             _transaction.Dispose();
         }
